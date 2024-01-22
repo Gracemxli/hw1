1410e592e0419057a56f898e4b7ecec6a2776f48
Grace Li <gracemxli51@gmail.com>
2024-01-22 09:50:12 -0800

    lihk

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -4,10 +4,10 @@
 #include <sstream>
 
 #include "ulliststr.h"
-
+using namespace std;
 //Use this file to test your ulliststr implementation before running the test suite
 void printULL(ULListStr& name){
-
+    cout <<"hi";
 }
 int main(int argc, char* argv[])
 {
